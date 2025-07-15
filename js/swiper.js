c4dc4fc668b8d42ab1dc30b9462ce55d4d22cfa3
yiyingtec <122464329+yiyingtec@users.noreply.github.com>
2025-07-15 14:51:55 +0800

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 10, // 图片间距
   loop: true, // 无限循环
   autoplay: {
-    delay: 2000, // 2秒切换一次
+    delay: 2, // 2秒切换一次
     disableOnInteraction: false, // 用户操作后仍自动播放
   },
 });
