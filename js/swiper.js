3ad9f3886414892c63ef0e324682bee35955ac45
yiyingtec <122464329+yiyingtec@users.noreply.github.com>
2025-07-15 14:03:50 +0800

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,9 @@
-
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto', // 自动计算显示几张图片
+  spaceBetween: 10, // 图片间距
+  loop: true, // 无限循环
+  autoplay: {
+    delay: 2000, // 2秒切换一次
+    disableOnInteraction: false, // 用户操作后仍自动播放
+  },
+});
